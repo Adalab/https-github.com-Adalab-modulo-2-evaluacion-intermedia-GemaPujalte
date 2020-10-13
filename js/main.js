1aa79d42d568cf4e1d5eb3ec42aa9a008d347e85
GemaPujalte <gema.pujalte.iglesias@gmail.com>
2020-10-13 11:39:21 +0200

    add js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
 "use strict";
+
+const buttonElement = document.querySelector(".button");
+const inputNumber = document.querySelector(".js-button__number");
+const inputClue = document.querySelector(".js-clue");
+
+function getRandomNumber(max) {
+  return Math.ceil(Math.random() * max);
+}
+
+function getNumber() {
+  const randomNumber = getRandomNumber(100);
+  console.log(randomNumber);
+  const numberValue = inputNumber.value; //valor del input donde escribo el numero
+  inputClue.value = "Demasiado alto";
+
+  if (numberValue > randomNumber) {
+    inputClue.innerHTML = inputClue.value = "Demasiado alto";
+  } else if (numberValue < randomNumber) {
+    inputClue.innerHTML = inputClue.value = "Demasiado bajo";
+  }
+}
+
+buttonElement.addEventListener("click", getNumber);
