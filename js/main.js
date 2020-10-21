72ca8a7704199c44a0a8e2654b3758e3299a9db6
GemaPujalte <gema.pujalte.iglesias@gmail.com>
2020-10-21 21:29:34 +0200

    add exercise complete

--- js/main.js
+++ js/main.js
@@ -1,26 +1,50 @@
 "use strict";
 
-const buttonElement = document.querySelector(".button");
-const inputNumber = document.querySelector(".js-button__number");
-const inputClue = document.querySelector(".js-clue");
+const buttonElement = document.querySelector(".js-button");
+const inputNumber = document.querySelector(".js-inputNumber");
+const clueElement = document.querySelector(".js-clue");
+const attempsElement = document.querySelector(".js-attemps");
 
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
+const randomNumber = getRandomNumber(100); //esto va fuera de la funcion.
+console.log(`El número aleatorio es ${randomNumber}`);
 
-function getNumber() {
-  const randomNumber = getRandomNumber(100);
-  console.log(randomNumber);
-  const numberValue = inputNumber.value; //valor del input donde escribo el numero
-  inputClue.value = "Demasiado alto";
+function getNumber(event) {
+  event.preventDefault();
 
-  if (numberValue > randomNumber) {
-    inputClue.innerHTML = inputClue.value = "Demasiado alto";
-  } else if (numberValue < randomNumber) {
-    inputClue.innerHTML = inputClue.value = "Demasiado bajo";
-  } else if (numberValue === randomNumber) {
-    inputClue.innerHTML = inputClue.value = "Demasiado Has ganado campeona!!";
+  const numberValue = parseInt(inputNumber.value); //valor del input donde escribo el numero
+
+  /*TAMBIEN ENCONTRÉ ESTA MANERA DE COMPROBAR QUE ES UN NÚMERO LO QUE PONE EL USUARIO */
+  // if (isNaN(numberValue)) {
+  //   clueElement.innerHTML = "Tienes que introducir un número";
+  //   return;
+  // }
+  if (!numberValue) {
+    clueElement.innerHTML = "Tienes que introducir un número";
+    return;
+  }
+  console.log(numberValue);
+
+  if (numberValue >= 1 && numberValue <= 100) {
+    if (numberValue > randomNumber) {
+      clueElement.innerHTML = "Demasiado alto";
+    } else if (numberValue < randomNumber) {
+      clueElement.innerHTML = "Demasiado bajo";
+    } else if (numberValue === randomNumber) {
+      clueElement.innerHTML = "Has ganado campeona!!";
+    }
+  } else {
+    clueElement.innerHTML = "El número debe estar entre 1 y 100";
   }
 }
 
+let count = 0;
+function numbersOfAttemps() {
+  count++;
+  attempsElement.innerHTML = `Número de intentos: ${count}`;
+}
+
 buttonElement.addEventListener("click", getNumber);
+buttonElement.addEventListener("click", numbersOfAttemps);
